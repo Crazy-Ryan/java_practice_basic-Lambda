77a7b2a9e54ee53c2ef25f416e0f9bb338a8a12f
Ryan.Wang <ranwang1995@outlook.com>
2020-03-04 15:46:21 +0800

    [WangRan] feat: implement user defined functional interface OwnConsumer

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -1,4 +1,15 @@
 package com.thoughtworks;
 
 public class App {
+    public static void main(String[] args) {
+        System.out.println("---user defined functional interface--");
+        String testStr = "add * to each end of this string";
+        System.out.println(testStr);
+        ownConsumerPrint(testStr);
+    }
+
+    public static void ownConsumerPrint(String text){
+        OwnConsumer ownConsumer = str -> System.out.println("*" + str + "*");
+        ownConsumer.print(text);
+    }
 }

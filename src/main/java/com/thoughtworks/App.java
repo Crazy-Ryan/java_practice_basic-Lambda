7f064402f7198bfb9e2730ff148a205759288b53
Ryan.Wang <ranwang1995@outlook.com>
2020-03-04 15:51:40 +0800

    [WangRan] feat: implement predefined functional interface consumer

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -1,15 +1,25 @@
 package com.thoughtworks;
 
+import java.util.function.Consumer;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("---user defined functional interface--");
         String testStr = "add * to each end of this string";
+        System.out.println("---user defined functional interface--");
         System.out.println(testStr);
         ownConsumerPrint(testStr);
+        System.out.println("---predefined functional interface--");
+        System.out.println(testStr);
+        consumerPrint(testStr);
     }
 
     public static void ownConsumerPrint(String text){
         OwnConsumer ownConsumer = str -> System.out.println("*" + str + "*");
         ownConsumer.print(text);
+    }
+
+    public static void consumerPrint(String text){
+        Consumer<String> consumer = str -> System.out.println("*" + str + "*");
+        consumer.accept(text);
     }
 }
